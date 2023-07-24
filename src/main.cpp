b2dae74b9a9c37c4b894df81a348a85db319d0a0
bl33h <per21371@uvg.edu.gt>
2023-07-24 02:15:05 -0600

    main method

--- src/main.cpp
+++ src/main.cpp
@@ -227,3 +227,48 @@ void render(SDL_Renderer* renderer) {
     newBoard();
     SDL_RenderPresent(renderer);
 }
+
+int main() {
+    srand(time(NULL));
+    initializeBoard();
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow("Conway's Game of Life", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED
+    );
+    if (renderer == nullptr) {
+        std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+        render(renderer);
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
